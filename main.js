882e363b74232bbcffaf12a4b3bb67bd70cfa86c
Jungmin Ahn <jungminahn829@gmail.com>
2024-02-20 23:07:59 +1100

    02_commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `51e42bfd149e42b09848c52a9e31b23e`;
 let news = []; //전역변수로 선언
 const getLatestNews = async () => {
     // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`); //URL (instance)
+    const url = new URL(`https://newstimes-min.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`); //URL (instance)
 
     const response = await fetch(url); //데이터 받을때까지 fetch() 기다려줌
     const data = await response.json();
