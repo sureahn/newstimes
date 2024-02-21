3f0f5900ee5d73bf5771767ca0778d7cd63c70be
Jungmin Ahn <jungminahn829@gmail.com>
2024-02-22 03:48:15 +1100

    03_commit

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
 const API_KEY = `51e42bfd149e42b09848c52a9e31b23e`;
-let news = []; //전역변수로 선언
+let newsList = []; // 전역 변수로 선언
 const getLatestNews = async () => {
-    // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-    const url = new URL(`https://newstimes-min.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`); //URL (instance)
+  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL(
+    `https://newstimes-min.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
+  ); // URL (instance)
 
-    const response = await fetch(url); //데이터 받을때까지 fetch() 기다려줌
-    const data = await response.json();
-    news = data.articles; //news는 다른곳에서도 쓸 예정. 전역변수로 선언 후 재할당
-    console.log("sss", news);
+  const response = await fetch(url); // 데이터 받을 때까지 fetch() 기다려줌
+  const data = await response.json();
+  newsList = data.articles; // news는 다른 곳에서도 쓸 예정. 전역 변수로 선언 후 재할당
+  render();
+  console.log("sss", newsList);
+};
+
+const render = () => {
+  // UI 그려줌
+  const newsHTML = newsList
+    .map((news) => {
+      const image = news.urlToImage ? news.urlToImage : "Image Not Available";
+      const content = news.description ? news.description : "No content";
+      return ` 
+        <div class="row news row-gap-3">
+            <div class="col-lg-4">
+                <img class="news-img-size" src=${news.urlToImage} alt="" />
+            </div>
+            <div class="col-lg-8">
+                <h2 id="news-title">${news.title}</h2>
+                <p id="news-content">${content}</p>
+                <div class="">${news.source.name} / ${news.publishedAt}</div>
+            </div>
+        </div>`;
+    })
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML; // news-board에 붙임
+};
+
+// sidemenubar
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
 };
 
 getLatestNews();
